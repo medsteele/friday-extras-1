dcb9aeb0dfe914d544ad6f956a2ed6725be6158f
Mark Steele <steele.mark.ed@gmail.com>
2022-06-24 18:53:01 -0400

    Update scripts.js

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -2,19 +2,17 @@
 const progress1 = document.getElementById("progress1");
 
 document.getElementById("button1").addEventListener("click", () => {
-    let interval = setInterval(() => {
+    setInterval(() => {
         progress1.value += 5;
-    }, 1000);
+    }, 1000);    
 });
 
 document.getElementById("button2").addEventListener("click", () => {
-    clearInterval(interval);
     progress1.value = 0;
     alert('progress bar reset');
 });
 
 document.getElementById("button3").addEventListener("click", () => {
-    clearInterval(interval);
     progress1.value = progress1.value;
     alert('progress bar value = ' + progress1.value);
 });
