29715724f981ad2d934d5faf5d8e39043046f539
Mark Steele <steele.mark.ed@gmail.com>
2022-06-24 17:55:50 -0400

    misc experiment

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -4,10 +4,14 @@ const progress1 = document.getElementById("progress1");
 document.getElementById("button1").addEventListener("click", () => {
     setInterval(() => {
         progress1.value += 5;
-    }, 1000)
+    }, 1000);
 });
 
 document.getElementById("button2").addEventListener("click", () => {
-    progress1.value = 0
+    progress1.value = 0;
 });
 
+document.getElementById("button3").addEventListener("click", () => {
+    progress1.value = progress1.value;
+    alert('progress bar value = ' + progress1.value);
+})
