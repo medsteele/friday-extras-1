7e94e1171f9671d419416b65f69c4a716f7b213a
Mark Steele <steele.mark.ed@gmail.com>
2022-06-24 13:59:52 -0400

    Update scripts.js

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -2,10 +2,12 @@
 const progress1 = document.getElementById("progress1");
 
 document.getElementById("button1").addEventListener("click", () => {
-    progress1.value = 90
+    setInterval(() => {
+        progress1.value += 10;
+    }, 1000)
 });
 
 document.getElementById("button2").addEventListener("click", () => {
-    progress1.value = 10
+    progress1.value = 0
 });
 
