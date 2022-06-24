c8512b63eb366a8f150c983ce4a96fa9eba137e6
Mark Steele <steele.mark.ed@gmail.com>
2022-06-24 13:49:43 -0400

    updates

---
+++ docs/js/scripts.js
@@ -0,0 +1,8 @@
+
+document.getElementById("button1").addEventListener("click", () => {
+    alert('b1 clicked');
+});
+
+document.getElementById("button2").addEventListener("click", () => {
+    alert('b2 clicked');
+});
